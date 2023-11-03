a9d2d873cded62c9ae5a05addd33b4a56e90bcf1
MSA <muhammetsaitakgunes@gmail.com>
2023-11-03 17:52:17 +0300

    test

--- Frontend/HotelProject.WebUI/Controllers/StaffController.cs
+++ Frontend/HotelProject.WebUI/Controllers/StaffController.cs
@@ -4,6 +4,13 @@ namespace HotelProject.WebUI.Controllers
 {
     public class StaffController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public StaffController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
         public IActionResult Index()
         {
             return View();
